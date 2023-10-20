2aa4b045e844877465e7826ecb295f491217b2f6
Shrawan Sapre <shrawansapre@gmail.com>
2023-10-20 03:03:33 -0400

    fixed map popup UI

--- src/components/ShopPopup.js
+++ src/components/ShopPopup.js
@@ -1,6 +1,6 @@
 // CoffeeShopPopup.js
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Button, Link } from "@mui/material";
 import { Popup } from "react-map-gl";
 import styled from "@emotion/styled";
 
@@ -12,18 +12,28 @@ const PopupImage = styled.img`
 
 const ShopPopup = ({ selectedShop, onClose }) => (
     <Popup latitude={selectedShop.lat} longitude={selectedShop.lng} onClose={onClose} tipSize={1} anchor="top">
-        <div className="popup-header">
-            <PopupImage src={selectedShop.thumbnail} alt={selectedShop.name}/>
-            <Typography variant="h6">{selectedShop.name}</Typography>
-        </div>
-        <div className="popup-content">
-            <Typography variant="body">Rating: {selectedShop.rating}</Typography>
-            <Typography variant="body">Open Now: {selectedShop.openingHours} </Typography>
-            <Typography variant="body">Address: {selectedShop.address}</Typography>
-        </div>
-        <div className="popup-footer">
-            <a href={selectedShop.website} target="_blank" rel="noopener noreferrer">Learn More</a>
-        </div>
+   <div className="popup-header">
+        <PopupImage src={selectedShop.thumbnail} alt={selectedShop.name}/>
+        <Typography variant="h6" style={{ margin: '0 10px' }}>{selectedShop.name}</Typography>
+    </div>
+    <div className="popup-content" style={{ padding: '10px' }}>
+        <Typography variant="body2">
+          <span className="icon-rating"></span> Rating: {selectedShop.rating}
+        </Typography>
+        <Typography variant="body2">
+          <span className="icon-clock"></span> Open Now: {selectedShop.openingHours}
+        </Typography>
+        <Typography variant="body2">
+          <span className="icon-location"></span> Address: {selectedShop.address}
+        </Typography>
+    </div>
+    <div style={{ textAlign: 'center' }}>
+    <Button variant="contained" size="small" sx={{backgroundColor: "#f4a261",color: "#ffffff",}}>
+      <Link href={selectedShop.gMapsLink} color="inherit" underline="none" target="_blank" rel="noopener noreferrer" aria-label="Visit shop">
+        Visit
+      </Link>
+    </Button>
+    </div>
     </Popup>
 );
 
