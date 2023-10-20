4a9b5fb595f527eeeee1b4f5b7a30f4e051f68e1
Shrawan Sapre <shrawansapre@gmail.com>
2023-10-20 03:01:36 -0400

    fixed Cards List UI

--- src/components/listCards.js
+++ src/components/listCards.js
@@ -8,8 +8,9 @@ import CardMedia from "@mui/material/CardMedia";
 import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import StarIcon from "@mui/icons-material/Star";
-import PlaceIcon from "@mui/icons-material/Place";
 import Button from "@mui/material/Button";
+
+import coffeePlaceholder from "../assets/coffee_thumbnail_placeholder.svg";
 
 import { CoffeeShopContext } from "../context/CoffeeShopContext";
 import LoadingAnimation from "./loadingAnimation";
@@ -19,62 +20,78 @@ const ListCards = () => {
   console.log("rendering List");
 
   return (
-    <div style={{marginTop:'20px'}}>
+    <div >
          {loading ? (
         <LoadingAnimation/>
       ) : (
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        
+      <Grid container spacing={{ xs: 2, md: 3 }} justifyContent="center" alignItems="center" style={{marginTop:"30px"}}>
       {coffeeShops.map((shop, index) => (
-       <Grid item xs={12} sm={12} md={12} key={index}>
-        <Card
-          key={index}
-          sx={{
-            maxWidth: 400,
-            marginBottom: 4,
-            borderRadius: "16px",
-            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <CardMedia
-            component="img"
-            alt={shop.name}
-            height="200"
-            image={shop.thumbnail}
-            sx={{
-              borderTopLeftRadius: "16px",
-              borderTopRightRadius: "16px",
-            }}
-          />
-          <CardContent sx={{ padding: "16px 24px" }}>
-            <Typography gutterBottom variant="h5" component="div">
-              {shop.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {shop.address}
-            </Typography>
-          </CardContent>
-          <CardActions
-            disableSpacing
-            sx={{ justifyContent: "space-between", padding: "0 24px 16px" }}
-          >
-            <IconButton aria-label="add to favorites">
-              <StarIcon />
-            </IconButton>
-            {/* <IconButton aria-label="place">
-              <PlaceIcon />
-            </IconButton> */}
-            <Button
-              variant="contained"
-              size="small"
-              sx={{ backgroundColor: "#f4a261", color: "#ffffff" }}
-            >
-              <Link href={shop.gMapsLink} color="inherit" underline="none" target="_blank" rel="noopener noreferrer">
-                Visit
-              </Link>
-            </Button>
-          </CardActions>
-        </Card>
+       <Grid item xs={12} sm={12} md={4} lg={4} key={index}>
+    <Card
+  key={index}
+  sx={{
+    maxWidth:'400px',
+    maxHeight: '500px',
+    margin: 'auto',
+    borderRadius: "16px",
+    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)"
+  }}
+>
+  <CardMedia
+    component="img"
+    alt={shop.name}
+    height="200"
+    image={shop.thumbnail || coffeePlaceholder}  // Fallback image
+    sx={{
+      borderTopLeftRadius: "16px",
+      borderTopRightRadius: "16px",
+      objectFit: shop.thumbnail ? "none":"contain" 
+    }}
+  />
+  <CardContent sx={{ padding: ["8px 16px", "16px 24px"] }}>
+    <Typography gutterBottom variant="h5" component="div" sx={{fontFamily: "'Patrick Hand', serif"}}>
+      {shop.name}
+    </Typography>
+    <Typography variant="body2" color="text.secondary">
+      {shop.address}
+    </Typography>
+  </CardContent>
+  <CardActions
+    disableSpacing
+    sx={{
+      justifyContent: "space-between",
+      padding: "0 24px 16px",
+      '& button': {
+        '&:hover': {
+          backgroundColor: '#f4a261',
+        }
+      }
+    }}
+  >
+    <IconButton aria-label="add to favorites">
+      <StarIcon />
+    </IconButton>
+    <Button
+      variant="contained"
+      size="small"
+      sx={{
+        backgroundColor: "#f4a261",
+        color: "#ffffff",
+      }}
+    >
+      <Link
+        href={shop.gMapsLink}
+        color="inherit"
+        underline="none"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit shop"
+      >
+        Visit
+      </Link>
+    </Button>
+  </CardActions>
+</Card>
       </Grid>
       
       ))}
