5609ff78907f67e045a93053ef103f9f96023c18
Shrawan Sapre <shrawansapre@gmail.com>
2023-10-20 20:23:09 -0400

    added website, rating, open/close to Cards

--- src/components/listCards.js
+++ src/components/listCards.js
@@ -8,6 +8,9 @@ import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import StarIcon from "@mui/icons-material/Star";
 import Button from "@mui/material/Button";
+import Rating from '@mui/material/Rating';
+import LanguageIcon from '@mui/icons-material/Language'
+
 
 import coffeePlaceholder from "../assets/coffee_thumbnail_placeholder.svg";
 import LoadingAnimation from "./loadingAnimation";
@@ -27,15 +30,37 @@ const ListCards = () => {
           <Card key={index} sx={{maxWidth:'400px', maxHeight: '500px', margin: 'auto', borderRadius: "16px", boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)"}}>
             <CardMedia component="img" alt={shop.name} height="200" image={shop.thumbnail || coffeePlaceholder} sx={{ borderTopLeftRadius: "16px", borderTopRightRadius: "16px", objectFit: shop.thumbnail ? "none":"contain"}}/>
             <CardContent sx={{ padding: ["8px 16px", "16px 24px"] }}>
-              <Typography gutterBottom variant="h5" component="div">{shop.name}</Typography>
-              <Typography variant="body2" color="text.secondary">{shop.address}</Typography>
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                {/* Name of shop */}
+                <Typography gutterBottom variant="h5" component="div">{shop.name}</Typography>
+                {/* Open/Close */}
+                <div style={{ border: '1px solid', borderColor: shop.open_now ? 'green' : 'red', color: shop.open_now ? 'green' : 'red', borderRadius: '4px', padding: '2px 8px',fontWeight: 'bold',fontSize: '0.8em'}}>
+                  {shop.open_now ? 'Open' : 'Close'}
+                </div>
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+              {/* Rating */}
+              <Rating name="read-only" value={shop.rating} size="medium" readOnly precision={0.1} />
+              {shop.rating ? (
+              <Typography variant="body2" color="text.secondary" style={{ marginLeft: '5px' }}>{shop.rating} ({shop.user_ratings_total} reviews)</Typography>) 
+              :  <Typography variant="body2" color="text.secondary" style={{ marginLeft: '5px' }}>Rating not available</Typography>}
+              </div>
+              {/* Website */}
+              {shop.website && (
+                <div style={{ marginTop: '6px', display: 'flex', alignItems: 'center',  }}>
+                <Link href={shop.website} color="text.secondary" underline="none" target="_blank" rel="noopener noreferrer" style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
+                  <LanguageIcon fontSize="small" sx={{marginRight:"5px"}}/>
+                  <Typography variant="body2" color="text.secondary">{shop.website}</Typography>
+                </Link>
+              </div>
+              )}           
             </CardContent>
             <CardActions disableSpacing sx={{ justifyContent: "space-between", padding: "0 24px 16px",'& button': {'&:hover': {backgroundColor: '#f4a261'}}}}>
               <IconButton aria-label="add to favorites">
                 <StarIcon />
               </IconButton>
               <Button variant="contained" size="small" sx={{ backgroundColor: "#f4a261",color: "#ffffff"}}>
-                <Link href={`https://www.google.com/maps/search/?api=1&query=Google&query_place_id=${shop.id}`} color="inherit" underline="none" target="_blank" rel="noopener noreferrer" aria-label="Visit shop">Visit</Link>
+                <Link href={shop.google_maps_url} color="inherit" underline="none" target="_blank" rel="noopener noreferrer" aria-label="Visit shop">Visit</Link>
               </Button>
             </CardActions>
           </Card>
