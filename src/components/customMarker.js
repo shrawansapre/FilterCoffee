261bd43185f1ec4524f6575b077311fbaf2952ab
Shrawan Sapre <shrawansapre@gmail.com>
2023-10-20 20:22:41 -0400

    added relevant info to markers

--- src/components/customMarker.js
+++ src/components/customMarker.js
@@ -1,13 +1,18 @@
 import * as React from "react";
 import coffeeIcon from "../assets/coffeeIcon.svg";
 
-function CustomMarker() {
+function CustomMarker({details}) {
   return (
-    <img
-      src={coffeeIcon}
-      alt="Custom marker"
-      style={{ width: "24px", height: "24px" }}
-    />
+    <div style={{ position: "relative", textAlign: "center" }}>
+      {/* Coffee Icon */}
+      <img src={coffeeIcon} alt="Coffee marker" style={{ width: "24px", height: "24px" }}/>
+      <div style={{position: "absolute", top: 0,left: "50%", transform: "translate(-50%, -100%)", backgroundColor: "#fff", borderRadius: "5px", paddingRight: "5px",display: "flex",alignItems: "center"}}>
+        {/* Rating */}
+        <div style={{ marginLeft: "5px",marginRight: "5px",padding: "2px 4px"}}>{details.rating ? details.rating : "N/A"}</div>
+        {/* Open/Close Sign */}
+        <div style={{ width: "10px",height: "10px",borderRadius: "50%",backgroundColor: details.open_now ? "green" : "red"}}></div>
+    </div>
+    </div>
   );
 }
 
